e1a8e01a646583d917cee02f397e4e2dc777732a
Luqman_Reburn <46706073+LuqmanReburn@users.noreply.github.com>
2020-12-09 21:41:20 +0800

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -38,7 +38,7 @@ async def help_message_f(client, message):
     #message_id = 99
     # display the /help
     
-    await message.reply_text("""join this group forr help-- @GbotStoreSupport\n\n And also don't forget to fork this repo: <a href="https://github.com/gautamajay52/TorrentLeech-Gdrive">TorrentLeech-Gdrive</a>""", disable_web_page_preview=True)
+    await message.reply_text("""Thanks for joining... If you need some help tag me @Oreooo""", disable_web_page_preview=True)
 
 
 # async def rename_message_f(client, message):
@@ -51,7 +51,7 @@ async def help_message_f(client, message):
 #     ])
 #     reply_markup = pyrogram.InlineKeyboardMarkup(inline_keyboard)
 #     await message.reply_text(
-#         "please use @renamebot",
+#         "please use @OreoooRenamer_bot",
 #         quote=True,
 #         reply_markup=reply_markup
 #     )
